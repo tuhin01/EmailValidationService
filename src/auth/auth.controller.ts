d355f3fde7735e2fc26b8bb2e7d0406019d13f3d
Billal Hossain (Tousif) <tuhin.world@gmail.com>
2025-01-30 14:05:10 +0600

    Update auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 import { SkipThrottle } from '@nestjs/throttler';
-import { EmailDto } from '../domains/dto/email.dto';
 
 @Controller('auth')
 export class AuthController {
